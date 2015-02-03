4aadc96a578d0c570083b8fa9fac2e13873c9dec
Brenda Jin <brendajin@gmail.com>
2015-02-02 17:38:20 -0800

    updated variable names, also made a small edit so that array of length 1 is covered

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -4,33 +4,35 @@ function binarySearch(array, elem) {
 	var rangeStart = 0,
 		rangeEnd = array.length,
 		rangeLength = rangeEnd - rangeStart,
-		halfway = parseInt((rangeLength)/2, 10),
-		distFrom0 = 0 + halfway;
+		rangeMid = parseInt((rangeLength)/2, 10),
+		index = 0 + rangeMid;
 
-	while(rangeLength > 1) {
+	while(rangeLength >= 1) {
 		// check whether we have found the element or checked all the elements
-		if(array[distFrom0] == elem) { // found the element
-			return distFrom0;
-		} else if(array[distFrom0 - 1] == elem) { // found the element
-			return distFrom0 - 1;
+		if(array[index] == elem) { // found the element
+			return index;
+		} else if(array[index - 1] == elem) { // found the element
+			return index - 1;
 		} else if(rangeLength <= 2) { // checked all the elements
-			return false;
+			return -1;
 		}
 
 		// if we haven't found it, and if we haven't checked them all
-		if(array[distFrom0] > elem){ // if we need to search below
-			rangeEnd = distFrom0 - 1;
+		if(array[index] > elem){ // if we need to search below
+			rangeEnd = index - 1;
 		} else { // if we need to search above
-			rangeStart = distFrom0 + 1;
+			rangeStart = index + 1;
 		}
 
 		// get the new range
 		rangeLength = rangeEnd - rangeStart;
 
-		// find the halfway point
-		halfway = parseInt((rangeLength)/2, 10);
+		// find the rangeMid point
+		rangeMid = parseInt((rangeLength)/2, 10);
 
 		// get the distance from 0
-		distFrom0 = rangeStart + halfway;
+		index = rangeStart + rangeMid;
 	}
+
+	return -1;
 }
